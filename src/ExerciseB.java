8ca0c5ea72476e9ccfc5c870c142a0dc787ba44f
luisrodrigues <luis.cnm@gmail.com>
2020-01-20 16:59:38 +0000

    class resolution

--- src/ExerciseB.java
+++ src/ExerciseB.java
@@ -1,39 +1,56 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ExerciseB {
     public static final int NUM_ITER = 1000;
     public static final int NUM_ELEMENTS = 5;
 
     static class MyThread extends Thread {
-        private Map<Integer, Integer> database;
+        private Map<Integer, AtomicInteger> database;
         private Random numGenerator;
 
-        MyThread (Map<Integer, Integer> database) {
+        MyThread(Map<Integer, AtomicInteger> database) {
             this.database = database;
             this.numGenerator = new Random();
         }
 
-        public void run () {
+        public void run() {
             for (int i = 0; i < NUM_ITER; i++) {
                 //select an element to change
                 int id = numGenerator.nextInt(NUM_ELEMENTS);
 
                 // add/update the element to the database
-                if(database.containsKey(id)){
+
+                //ConcurrentHashMap methods
+                database.putIfAbsent(id, new AtomicInteger(0));
+                database.get(id).incrementAndGet();
+
+                //synchronized (database) { //only allows one visit to the DB -> synchronizedHashMap
+                /*
+                if (database.containsKey(id)) {
                     //update the element
-                    Integer element = database.get(id);
-                    element+=1;
+                    AtomicInteger element = database.get(id);
+                    element.incrementAndGet();
                     database.put(id, element);
-                }
-                else{
+                } else {
                     //create the element
-                    database.put(id, 1);
+                    database.put(id, new AtomicInteger(1));
                 }
+                */
             }//for
         }//run
+
     }//Threadclass
 
     public static void main (String[] args) throws Exception {
-        Map<Integer, Integer> DB = new HashMap<Integer, Integer>();
+        //Map<Integer, Integer> DB = new HashMap<Integer, Integer>();
+        //Map<Integer, Integer> DB = Collections.synchronizedMap(new HashMap<Integer, Integer>());
+        Map<Integer, AtomicInteger> DB = new ConcurrentHashMap<Integer, AtomicInteger>(); // use AtomicInteger
+
         Thread a = new MyThread(DB);
         Thread b = new MyThread(DB);
 
@@ -46,10 +63,10 @@ public class ExerciseB {
         // sum the elements in the map
         int total = 0;
         for(int i=0; i < NUM_ELEMENTS; i++) {
-            Integer el = DB.get(i);
+            AtomicInteger el = DB.get(i);
             if (el != null){
                 System.out.println("Elements in bucket #"+i+":"+el);
-                total += DB.get(i);
+                total += DB.get(i).get();
             }
         }//for
         System.out.println("Total items:"+total);
